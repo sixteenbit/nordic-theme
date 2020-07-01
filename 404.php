215865ce17a28248407adec060ed9f9c0306f831
Justin Peacock <justin@sixteenbit.com>
2020-07-01 14:58:25 -0400

    footer widgets padding and other styling

--- 404.php
+++ 404.php
@@ -12,11 +12,11 @@ get_header();
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found text-center">
-				<header class="page-header padding-top-3">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'nordic-theme' ); ?></h1>
+				<header class="entry-header">
+					<h1 class="entry-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'nordic-theme' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content grid-container">
+				<div class="entry-content grid-container">
 					<div class="grid-x grid-padding-x">
 						<div class="cell small-12 medium-10 medium-offset-1 large-8 large-offset-2">
 							<p class="lead"><?php esc_html_e( 'The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site\'s homepage and see if you can find what you are looking for.', 'nordic-theme' ); ?></p>
