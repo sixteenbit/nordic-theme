753c1665dc442e67aefd4e3071f0d61babf3728a
Justin Peacock <justin@sixteenbit.com>
2020-07-30 14:54:29 -0400

    a few fixes for release

--- 404.php
+++ 404.php
@@ -21,7 +21,7 @@ get_header();
 						<div class="cell small-12 medium-10 medium-offset-1 large-8 large-offset-2">
 							<p class="lead"><?php esc_html_e( 'The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site\'s homepage and see if you can find what you are looking for.', 'nordic-theme' ); ?></p>
 
-							<a class="button" href="<?php echo site_url(); ?>"><i class="fal fa-long-arrow-left"></i> <?php esc_html_e( 'Back Home', 'nordic-theme' ); ?></a>
+							<a class="button" href="<?php echo site_url(); ?>"><i class="fas fa-long-arrow-left"></i> <?php esc_html_e( 'Back Home', 'nordic-theme' ); ?></a>
 						</div>
 					</div>
 				</div><!-- .page-content -->
